14cc8bda78fea9a73469037967979573b6bf6d19
Sumit Mahalle <sumitmahalle25@gmail.com>
2026-07-24 12:11:55 +0000

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
